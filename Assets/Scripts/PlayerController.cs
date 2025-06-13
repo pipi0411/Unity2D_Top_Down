8518a5f1287a02f78b26c63ff871f3de8d9cf7ba
pipi0411 <20a41010217@students.hou.edu.vn>
2025-06-13 23:53:09 +0700

    update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,56 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float moveSpeed = 1f;
+    private PlayerControls playerControls;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerControls = new PlayerControls();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+    private void Update()
+    {
+        PlayerInput();
+    }
+    private void FixedUpdate()
+    {
+        Move();
+        AdjustPlayerFacingDirection();
+    }
+    private void PlayerInput()
+    {
+        movement = playerControls.Movement.Move.ReadValue<Vector2>();
+        animator.SetFloat("moveX", movement.x);
+        animator.SetFloat("moveY", movement.y);
+    }
+    private void Move()
+    {
+        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePos = Mouse.current.position.ReadValue();
+        Vector3 playerScreenPos = Camera.main.WorldToScreenPoint(transform.position);
+        if (mousePos.x < playerScreenPos.x)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+
 }
