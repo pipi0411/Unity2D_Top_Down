9c24fcc1c79cfab481b7bd1e5bcf907ba79bbc59
Pupa06 <20a41010217@students.hou.edu.vn>
2025-08-25 20:26:39 +0700

    update pick up coin

---
+++ Assets/Scripts/PickUpSpawner.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class PickUpSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject goldCoinPrefab;
+    public void DropItems()
+    {
+        Instantiate(goldCoinPrefab, transform.position, Quaternion.identity);
+    }
+}
